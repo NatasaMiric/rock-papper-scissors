2cef85bb2c54b3e6cc55215865602189d4a5211e
NatasaMiric <natasa.miric94@gmail.com>
2022-10-21 19:16:44 +0000

    Set up structure for basic game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,19 @@
+/*
+ 1. user select option
+ 2. generate computer selection
+ 3. display user and comp choice and message who is the winner
+ 4. update score
+ */
 
+function userInput() {
 
+}
+
+function generateComputerInput() {
+
+}
+
+function checkWinner() {
+
+}
 
