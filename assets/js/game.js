b99bbf568512c1fc03f1dff348377e3e2fc701dd
NatasaMiric <natasa.miric94@gmail.com>
2022-10-22 14:22:47 +0000

    Select and store all elements in use and add iteration and event listeners for buttons

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,13 +4,31 @@
  3. display user and comp choice and message who is the winner
  4. update score
  */
+ 
+ // Target and store all variables in use  
 
-function userInput() {
+ const btnChoices= document.querySelectorAll(".btn");
+ const resultMessage= document.getElementById("result-text");
+ const userInput= document.getElementById("player-choice");
+ const compInput= document.getElementById("computer-choice");
+ let userChoice;
+ let compChoice;
+ let result;
+ 
+ /* Select all three buttons and add event listeners so that everytime 
+ when player clicks on the button the id gets extracted.
+ Add the function that generates computer's choice and 
+ the function that checks who is the winner
+ */
+ btnChoices.forEach( button => button.addEventListener('click', () => {
 
-}
+    userChoice= button.id;   
+    generateComputerInput();
+    checkWinner();
+ })); 
 
 function generateComputerInput() {
-
+    
 }
 
 function checkWinner() {
