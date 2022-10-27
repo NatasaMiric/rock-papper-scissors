81c9a6f7888e29ac7749c72b203b3a1439dc4492
NatasaMiric <natasa.miric94@gmail.com>
2022-10-27 14:33:38 +0000

    Refactor the checkWinner function in js file

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,32 +1,20 @@
-/*
- 1. user select option
- 2. generate computer selection
- 3. display user and comp choice and message who is the winner
- 4. update score
- */
-
-// Target and store all variables in use  
+ 
 
 const btnChoices = document.querySelectorAll(".btn");
-const resultMessage = document.getElementById("result-text");
+
 const userInput = document.getElementById("player-choice");
 const compInput = document.getElementById("computer-choice");
 let playerScore = document.getElementById("player-score");
 let compScore = document.getElementById("comp-score");
 let userChoice;
 let compChoice;
-let result;
+
 let pScore= 0;
 let cScore= 0;
 
 
 
-/* Select all three buttons, iterate through buttons and add event listeners so that everytime 
-when player clicks on the button the id gets targeted and then it gets displayed 
-in  player's choice field with the use of inner.HTML property.
-Add the function that generates computer's choice and 
-the function that checks who is the winner. 
-*/
+
 btnChoices.forEach(button => button.addEventListener('click', () => {
 
     userChoice = button.id;
@@ -35,11 +23,7 @@ btnChoices.forEach(button => button.addEventListener('click', () => {
     checkWinner();
 }));
 
-/* Math.random will give us a random number between 1 and 3 and Math.floor an integer.
-Set value for each number.If computer randomly picks 1 his value is rock, if it picks 2,
-it is papper and 3 scissors. Assign the value to compChoice and add innerHTML
-to be displayed on user's screen. 
-*/
+
 function generateComputerInput() {
 
     const randomNum = Math.floor(Math.random() * 3) + 1;
@@ -59,46 +43,29 @@ function generateComputerInput() {
     compInput.innerHTML = compChoice;
 }
 
-/* Compare the user and computers input,inform user if he wins or loses and increment the score
-of the winner.
- Assign the inner.HTML property to result message player and computer score in order to be displayed on user's screen.
- */
-function checkWinner() {
+function isUserWinner(userChoice, compChoice) {
+    return (userChoice === "rock" && compChoice === "scissors")
+    || (userChoice === "paper" && compChoice === "rock") 
+    || (userChoice === "scissors" && compChoice === "paper");
+}
+
+function checkWinner(userChoice, compChoice) {
+    let result = null;
+    let pScore =  playerScore.innerHTML ;
+    let cScore = compScore.innerHTML;
 
     if (userChoice === compChoice) {
         result = "It's a Tie!";        
     }
-    if (userChoice === "rock" && compChoice === "paper") {
-
-        result = "You Loose!";
-        cScore++;        
+    else if (isUserWinner()) {
+        result = "You Win!";
+        pScore++;        
+    } else {
+        result= "Yoo Loose!";
+        cScore++;
     }
-    if (userChoice === "rock" && compChoice === "scissors") {
-
-        result = " You Win!"; 
-        pScore++;       
-    }
-    if (userChoice === "paper" && compChoice === "scissors") {
-
-        result = "You Loose!"; 
-        cScore++;       
-    }
-    if (userChoice === "paper" && compChoice === "rock") {
-
-        result = "You Win!"; 
-        pScore++;       
-    }
-    if (userChoice === "scissors" && compChoice === "rock") {
-
-        result = "You Loose!";    
-        cScore++;    
-    }
-    if (userChoice === "scissors" && compChoice === "paper") {
-
-        result = "You Win!";   
-        pScore++;     
-    }
-
+    
+    let resultMessage = document.getElementById("result-text");
     resultMessage.innerHTML = result;
     playerScore.innerHTML = pScore;
     compScore.innerHTML = cScore;
