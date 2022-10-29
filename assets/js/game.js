b1f4c9f6a1c44926d894b92aafc909ee4d3faa64
NatasaMiric <natasa.miric94@gmail.com>
2022-10-29 14:10:21 +0000

    Add 2 more button options,style in css and make them work in js

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,8 +4,8 @@ let pScore = 0;
 let cScore = 0;
 // Generetes random number and displaying random computer choice
 function generateComputerInput() {    
-    let randomNum = Math.floor(Math.random() * 3);
-    const CHOICES = ['rock', 'paper', 'scissors'];
+    let randomNum = Math.floor(Math.random() * 5);
+    const CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
     compChoice = CHOICES[randomNum];
     let compInput = document.getElementById("computer-choice");
     compInput.innerHTML = compChoice;
@@ -15,7 +15,15 @@ function generateComputerInput() {
 function isUserWinner(userChoice, compChoice) {
     return (userChoice === "rock" && compChoice === "scissors")
     || (userChoice === "paper" && compChoice === "rock") 
-    || (userChoice === "scissors" && compChoice === "paper");
+    || (userChoice === "scissors" && compChoice === "paper")
+    || (userChoice === "rock" && compChoice === "lizard")
+    || (userChoice === "lizard" && compChoice === "spock")
+    || (userChoice === "spock" && compChoice === "scissors")
+    || (userChoice === "scissors" && compChoice === "lizard")
+    || (userChoice === "lizard" && compChoice === "paper")
+    || (userChoice === "paper" && compChoice === "spock")
+    || (userChoice === "spock" && compChoice === "rock")
+    || (userChoice === "paper" && compChoice === "spock");
 }
 // Compares user and computer choice and increments the score depending who wins
 function checkWinner(userChoice, compChoice) {
